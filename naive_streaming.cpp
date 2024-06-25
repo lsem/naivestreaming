c9b14d48e265d06baaddd874fa0dec6f0bc1f313
Liubomyr Semkiv <liubomyr.semkiv@ringteam.com>
2024-06-25 14:59:35 +0300

    Added logger

--- naive_streaming.cpp
+++ naive_streaming.cpp
@@ -18,6 +18,41 @@
 // VideoCapture specific depdendencies:
 #include <linux/videodev2.h>
 
+#include <format>
+#include <string_view>
+
+enum class LogLevel { debug, info, warning, error };
+
+namespace lsem_log_details {
+template <class... Args>
+void print_log(LogLevel level, std::string_view fmt, Args&&... args) {
+  auto label_fn = [](LogLevel level) {
+    switch (level) {
+      case LogLevel::debug:
+        return "DEBUG";
+      case LogLevel::info:
+        return "INFO";
+      case LogLevel::warning:
+        return "WARNING";
+      case LogLevel::error:
+        return "ERROR";
+      default:
+        return "LogLevel::<unknown>";
+    }
+  };
+  std::cout << label_fn(level) << ": "
+            << std::vformat(fmt, std::make_format_args(args...)) << "\n";
+}
+}  // namespace lsem_log_details
+#define LOG_DEBUG(FmtMsg, ...) \
+  lsem_log_details::print_log(LogLevel::debug, FmtMsg, ##__VA_ARGS__)
+#define LOG_INFO(FmtMsg, ...) \
+  lsem_log_details::print_log(LogLevel::info, FmtMsg, ##__VA_ARGS__)
+#define LOG_WARNING(FmtMsg, ...) \
+  lsem_log_details::print_log(LogLevel::warning, FmtMsg, ##__VA_ARGS__)
+#define LOG_ERROR(FmtMsg, ...) \
+  lsem_log_details::print_log(LogLevel::error, FmtMsg, ##__VA_ARGS__)
+
 using namespace std;
 
 /* **SUBTASKS**
@@ -64,6 +99,11 @@ class VideoCapture {
 };
 
 // Implenebtation based on Video4Linux.
+// Documentation:
+//  1) https://docs.kernel.org/4.20/media/v4l-drivers/index.html
+//  2) https://github.com/kmdouglass/v4l2-examples
+//  3)
+//  https://stackoverflow.com/questions/10634537/v4l2-difference-between-enque-deque-and-queueing-of-the-buffer
 class VideoCaptureImpl : public VideoCapture {
  public:
   explicit VideoCaptureImpl(std::filesystem::path video_dev_fpath)
@@ -73,7 +113,7 @@ class VideoCaptureImpl : public VideoCapture {
     // What if we enumrate video devices first and let user select the device.
     m_v4l_fd = open(m_video_dev_fpath.c_str(), O_RDWR);
     if (m_v4l_fd == -1) {
-      cerr << "ERROR: failed opening /dev/video0: " << strerror(errno) << "\n";
+      LOG_ERROR("failed opening /dev/video0: {}", strerror(errno));
       return false;
     }
     return true;
@@ -82,9 +122,9 @@ class VideoCaptureImpl : public VideoCapture {
  public:
   virtual ~VideoCaptureImpl() override {
     if (close(m_v4l_fd) == -1) {
-      cerr << "ERROR: failed closing v4l descriptor. Ignoring..\n";
+      LOG_ERROR("ERROR: failed closing v4l descriptor. Ignoring..");
     } else {
-      cout << "DEBUG: v4l closed, capture destructed\n";
+      LOG_ERROR("closed, capture destructed");
     }
   }
 
@@ -96,10 +136,50 @@ class VideoCaptureImpl : public VideoCapture {
       perror("Querying device capabilities");
       return;
     }
-    cout << "DEBUG: Driver: " << caps.driver << "\n";
-    cout << "DEBUG: Card: " << caps.card << "\n";
-    cout << "DEBUG: Bus Info: " << caps.bus_info << "\n";
-    cout << "DEBUG: Capabilities: " << caps.capabilities << "\n";
+    auto as_cstr = [](auto barr) {
+      return reinterpret_cast<const char*>(&barr[0]);
+    };
+    LOG_DEBUG(
+        "Capabilities:\n\tDriver: {}\n\tCard: {}\n\tBus Info: "
+        "{}\nCapabilities:",
+        as_cstr(caps.driver), as_cstr(caps.card), as_cstr(caps.bus_info));
+
+#define PROCESS_CAP(C)         \
+  if (caps.capabilities & C) { \
+    LOG_DEBUG("{}", #C);       \
+  }                            \
+  while (false)
+    PROCESS_CAP(V4L2_CAP_VIDEO_CAPTURE);
+    PROCESS_CAP(V4L2_CAP_VIDEO_OUTPUT);
+    PROCESS_CAP(V4L2_CAP_VIDEO_OVERLAY);
+    PROCESS_CAP(V4L2_CAP_VBI_CAPTURE);
+    PROCESS_CAP(V4L2_CAP_VBI_OUTPUT);
+    PROCESS_CAP(V4L2_CAP_SLICED_VBI_CAPTURE);
+    PROCESS_CAP(V4L2_CAP_SLICED_VBI_OUTPUT);
+    PROCESS_CAP(V4L2_CAP_RDS_CAPTURE);
+    PROCESS_CAP(V4L2_CAP_VIDEO_OUTPUT_OVERLAY);
+    PROCESS_CAP(V4L2_CAP_HW_FREQ_SEEK);
+    PROCESS_CAP(V4L2_CAP_RDS_OUTPUT);
+    PROCESS_CAP(V4L2_CAP_VIDEO_CAPTURE_MPLANE);
+    PROCESS_CAP(V4L2_CAP_VIDEO_OUTPUT_MPLANE);
+    PROCESS_CAP(V4L2_CAP_VIDEO_M2M_MPLANE);
+    PROCESS_CAP(V4L2_CAP_VIDEO_M2M);
+    PROCESS_CAP(V4L2_CAP_TUNER);
+    PROCESS_CAP(V4L2_CAP_AUDIO);
+    PROCESS_CAP(V4L2_CAP_RADIO);
+    PROCESS_CAP(V4L2_CAP_MODULATOR);
+    PROCESS_CAP(V4L2_CAP_SDR_CAPTURE);
+    PROCESS_CAP(V4L2_CAP_EXT_PIX_FORMAT);
+    PROCESS_CAP(V4L2_CAP_SDR_OUTPUT);
+    PROCESS_CAP(V4L2_CAP_META_CAPTURE);
+    PROCESS_CAP(V4L2_CAP_READWRITE);
+    PROCESS_CAP(V4L2_CAP_ASYNCIO);
+    PROCESS_CAP(V4L2_CAP_STREAMING);
+    PROCESS_CAP(V4L2_CAP_META_OUTPUT);
+    PROCESS_CAP(V4L2_CAP_TOUCH);
+    PROCESS_CAP(V4L2_CAP_IO_MC);
+    PROCESS_CAP(V4L2_CAP_DEVICE_CAPS);
+#undef PROCESS_CAP
   }
 
   virtual vector<std::unique_ptr<AbstractVideoFormatSpec>> enumerate_formats()
@@ -133,7 +213,8 @@ class VideoCaptureImpl : public VideoCapture {
     // and RTTI dependency anymore.
     auto* p = dynamic_cast<const Video4LinuxVideoFormat*>(&f);
     if (!p) {
-      cerr << "ERROR: passed wrong video format, unrelated type\n";
+      LOG_ERROR("passed wrong video format, unrelated type: {}",
+                strerror(errno));
       return false;
     }
 
@@ -145,12 +226,11 @@ class VideoCaptureImpl : public VideoCapture {
     fmt.fmt.pix.field = V4L2_FIELD_NONE;
 
     if (ioctl(m_v4l_fd, VIDIOC_S_FMT, &fmt) == -1) {
-      cerr << "ERROR: Could not set format description: " << strerror(errno)
-           << "\n";
+      LOG_ERROR("Could not set format description: {}", strerror(errno));
       return false;
     }
 
-    cout << "DEBUG: format selected\n";
+    LOG_DEBUG("format selected");
 
     return true;
   }
@@ -160,6 +240,27 @@ class VideoCaptureImpl : public VideoCapture {
     // In real system, this is something that stuff will hard code, because he
     // pr she knows what is available and what is needed, so here should also be
     // a method for querying possibilities.
+
+    struct v4l2_requestbuffers reqbuf;
+    struct {
+      void* start;
+      size_t length;
+    } * buffers;
+    unsigned int i;
+
+    memset(&reqbuf, 0, sizeof(reqbuf));
+    reqbuf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    reqbuf.memory = V4L2_MEMORY_MMAP;
+    reqbuf.count = 20;
+
+    // if (-1 == ioctl(fd, VIDIOC_REQBUFS, &reqbuf)) {
+    //   if (errno == EINVAL)
+    //     printf("Video capturing or mmap-streaming is not supported\\n");
+    //   else
+    //     perror("VIDIOC_REQBUFS");
+
+    //   exit(EXIT_FAILURE);
+    // }
   }
 
   virtual void stop() override {
@@ -189,7 +290,7 @@ class VideoCaptureImpl : public VideoCapture {
 std::unique_ptr<VideoCapture> make_video_capture(std::filesystem::path p) {
   auto impl = std::make_unique<VideoCaptureImpl>(std::move(p));
   if (!impl->initialize()) {
-    cerr << "ERROR: failed initializing video capture\n";
+    LOG_ERROR("failed initializing video capture");
     return nullptr;
   }
   return impl;
@@ -197,7 +298,7 @@ std::unique_ptr<VideoCapture> make_video_capture(std::filesystem::path p) {
 
 int main() {
   auto devs = VideoCaptureImpl::enumerate_video4_linux_devices();
-  cout << "DEBUG: Video4Linux devices:\n";
+  LOG_DEBUG("Video4Linux devices:");
   for (auto& x : devs) {
     cout << x << "\n";
   }
@@ -205,7 +306,7 @@ int main() {
   cout << "DEBUG: selecting first device in a list: " << devs.front() << "\n";
   auto capture = make_video_capture(devs.front());
   if (!capture) {
-    cout << "ERROR: failed creating videocapture. Exiting..\n";
+    LOG_ERROR("failed creating videocapture. Exiting..");
     return -1;
   }
 
@@ -214,7 +315,7 @@ int main() {
   capture->print_capabilities();
   auto formats = capture->enumerate_formats();
   if (formats.empty()) {
-    cerr << "ERROR: no available video formats\n";
+    LOG_ERROR("no available video formats");
     return -1;
   }
   capture->select_format(*formats.back());
@@ -225,7 +326,7 @@ int main() {
   asio::io_context ctx;
   asio::signal_set signals{ctx, SIGINT, SIGTERM};
   signals.async_wait([&ctx](std::error_code ec, int signal) {
-    cout << "DEBUG: closing app on request\n";
+    LOG_DEBUG("closing app on request");
     ctx.stop();
   });
 
