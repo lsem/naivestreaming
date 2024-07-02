0cbfdcf3da0c582efd7704740f172aed39a7ee20
Liubomyr Semkiv <sli.ukraine@gmail.com>
2024-07-02 18:45:06 +0300

    Got NAL limited by 1500 bytes; introduced Application class

--- encoder.hpp
+++ encoder.hpp
@@ -3,10 +3,19 @@
 #include <memory>
 #include "types.hpp"
 
+class EncoderClient {
+ public:
+  virtual ~EncoderClient() = default;
+
+  virtual void on_frame_started() = 0;
+  virtual void on_frame_ended() = 0;
+  virtual void on_nal_encoded(const uint8_t* data, size_t data_size) = 0;
+};
+
 class Encoder {
  public:
   virtual ~Encoder() = default;
   virtual void process_frame(BufferView& buff) = 0;
 };
 
-std::unique_ptr<Encoder> make_encoder();
+std::unique_ptr<Encoder> make_encoder(EncoderClient& client);
